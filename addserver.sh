7baa38eacbaaf79fdc6dceccc05b6d8ec884414a
antonieg <a.groenveld@st.hanze.nl>
2018-06-26 11:05:10 +0000

    Add_Nagios_Client_Script

---
+++ addserver.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+#Get server value
+x=2
+
+
+for i in 1 2
+do
+commandip="sudo salt 'UBU1604Minion-VM$x' cmd.run 'hostname -i'"
+ip=$(eval $commandip)
+
+IP="$(cut -d':' -f2 <<< $ip)"
+devicename="$(cut -d':' -f1 <<< $ip)"
+
+sudo touch /usr/local/nagios/etc/servers/$devicename.cfg 
+sudo chmod 755 /usr/local/nagios/etc/servers/$devicename.cfg 
+
+sudo echo " # Ubuntu Host configuration file
+define host {
+        use                          linux-server
+        host_name                    $devicename
+        alias                        Ubuntu Host
+        address                      $IP
+        register                     1
+}
+
+define service {
+      host_name                       $devicename
+      service_description             PING
+      check_command                   check_ping!100.0,20%!500.0,60%
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+}
+
+define service {
+      host_name                       $devicename
+      service_description             Check Users
+      check_command           check_local_users!20!50
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+}
+
+define service {
+      host_name                       $devicename
+      service_description             Local Disk
+      check_command                   check_local_disk!20%!10%!/
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+}
+
+define service {
+      host_name                       $devicename
+      service_description             Check SSH
+      check_command                   check_ssh
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+}
+
+define service {
+      host_name                       $devicename
+      service_description             Total Process
+      check_command                   check_local_procs!250!400!RSZDT
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+} " >> /usr/local/nagios/etc/servers/$devicename.cfg 
+((x=x+1))
+done
+
